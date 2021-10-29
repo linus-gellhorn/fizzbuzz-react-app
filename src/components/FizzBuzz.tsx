004b71088c829eb2769e17dba1a21a785bf1a4b8
Linus Gellhorn <linus.g@hotmail.com>
2021-10-29 11:31:58 +0100

    Added FizzBuzz functionality via helper function

--- src/components/FizzBuzz.tsx
+++ src/components/FizzBuzz.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import asFizzBuzz from "../utils/asFizzBuzz";
 
 function FizzBuzz(): JSX.Element {
-  return <h1>FizzBuzz app</h1>;
+  const [num, setNum] = useState<number>(1);
+  const [oldNums, setOldNums] = useState<(number | string)[]>([]);
+
+  const handleNextFizzBuzz = () => {
+    setNum(num + 1);
+    setOldNums([...oldNums, asFizzBuzz(num)]);
+  };
+
+  return (
+    <>
+      <h1>FizzBuzz app!</h1>
+      <p>FizzBuzz output: {oldNums.join(", ")}</p>
+      <button onClick={handleNextFizzBuzz}>Next</button>
+    </>
+  );
 }
 
 export default FizzBuzz;
---
+++ src/utils/asFizzBuzz.ts
@@ -0,0 +1,15 @@
+function asFizzBuzz(number: number): number | string {
+  let result;
+  if (number % 15 === 0) {
+    result = "FizzBuzz";
+  } else if (number % 5 === 0) {
+    result = "Buzz";
+  } else if (number % 3 === 0) {
+    result = "Fizz";
+  } else {
+    result = number;
+  }
+  return result;
+}
+
+export default asFizzBuzz;
